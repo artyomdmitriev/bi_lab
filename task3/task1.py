e0509a320e4b39c9411e6c0ef0cf65102ebbd49a
Artsemi Dzmitryieu <Artsemi_Dzmitryieu@epam.com>
2018-05-31 12:48:06 +0300

    Fix quotes

--- task3/task1.py
+++ task3/task1.py
@@ -4,7 +4,7 @@ print(list1)
 sliced_list1 = list1[::2]
 print(sliced_list1)
 
-list2 = [str(i) + "a" for i in range(1, 5)]
+list2 = [str(i) + 'a' for i in range(1, 5)]
 print(list2)
 
 print(list2.pop(1))
