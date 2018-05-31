4f0a12946155973c13505eefd203f23782a297e4
Artsemi Dzmitryieu <Artsemi_Dzmitryieu@epam.com>
2018-05-31 12:44:29 +0300

    Fix task1

--- task3/task1.py
+++ task3/task1.py
@@ -1,9 +1,14 @@
-list1 = ['ab', 'ac', 'ad', 'bb', 'bc', 'bd']
-sliced_list1 = list1[0::2]
+list1 = [a + b for a in 'ab' for b in 'bcd']
+print(list1)
+
+sliced_list1 = list1[::2]
 print(sliced_list1)
 
-list2 = ['1a', '2a', '3a', '4a']
+list2 = [str(i) + "a" for i in range(1, 5)]
+print(list2)
+
 print(list2.pop(1))
 
 list2_copy = list2.copy()
 list2_copy.append('2a')
+print(list2_copy)
