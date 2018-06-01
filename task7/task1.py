9ce202560e2580393e14de2ec7d389c846ae6077
Artsemi Dzmitryieu <Artsemi_Dzmitryieu@epam.com>
2018-06-01 21:14:45 +0300

    Improve Task1

--- task7/task1.py
+++ task7/task1.py
@@ -1,21 +1,21 @@
 import re
 
 # find all email addresses in string
-line = input("Enter string to find all emails: ")
+line = input("Enter a string to find email addresses: ")
 match = re.findall(r'[\w\.-]+@[\w\.-]+', line)
 print(match)
 
 # find all three, four, five characters long words in a string
-line = input('Enter string to words: ')
+line = input('Enter a string to find 3-, 4-, 5-characters long words: ')
 match = re.findall(r'\b[\w]{3,5}\b', line)
 print(match)
 
 # separate and print numbers of a given string
-line = input('Enter a string')
+line = input('Enter a string to find numbers: ')
 match = re.findall(r'\d+', line)
 print(match)
 
 # replace whitespaces with an underscore and vice versa
-line = input('Enter a string')
-print(re.sub(' ', '_', line))
+line = input('Enter a string to replace underscores and spaces: ')
+print(re.sub(r'\s', '_', line))
 print(re.sub('_', ' ', line))
