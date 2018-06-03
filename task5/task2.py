29e76787732542e7548d19f2ceee5a950c048194
Artsemi Dzmitryieu <Artsemi_Dzmitryieu@epam.com>
2018-06-03 16:22:44 +0300

    Impove json converting to other formats

--- task5/task2.py
+++ task5/task2.py
@@ -1,5 +1,5 @@
 import argparse
-import csv
+from csv import DictWriter
 import json
 import urllib.request
 # pip install xmltodict
@@ -9,32 +9,33 @@ import yaml
 
 def output_csv(file_name, content):
     with open(file_name + '.csv', 'w') as file:
-        writer = csv.writer(file)
-        for key, value in content.items():
-            writer.writerow([key, value])
+        writer = DictWriter(file, content[0].keys())
+        writer.writeheader()
+        writer.writerows(content)
 
 
 def output_json(file_name, content):
     with open(file_name + '.json', 'w') as file:
-        json.dump(content, file)
+        json.dump(content, file, indent=4)
 
 
-def output_yaml(file_name, content):
+def output_yml(file_name, content):
     with open(file_name + '.yml', 'w') as file:
         yaml.dump(content, file, default_flow_style=False)
 
 
 def output_xml(file_name, content):
     with open(file_name + '.xml', 'w') as file:
-        xmlString = xmltodict.unparse(content, pretty=True)
-        file.write(xmlString)
+        content_with_root = {'root': {'item': content}}
+        xml_string = xmltodict.unparse(content_with_root, pretty=True)
+        file.write(xml_string)
 
 
-# NOTE: it is expected that the called url returns a json
+# NOTE: it is expected that the called url returns an array in json
 parser = argparse.ArgumentParser()
 parser.add_argument('--download',
                     help='define the url of the file',
-                    default='https://transit.land/api/v1')
+                    default='https://api.nexchange.io/en/api/v1/currency/')
 parser.add_argument('--format',
                     help='define the acceptable file format',
                     default='json')
@@ -44,8 +45,9 @@ file_format = args.format
 
 content = urllib.request.urlopen(download_url)
 data = json.loads(content.read())
+
 if file_format == 'yml':
-    output_yaml('output', data)
+    output_yml('output', data)
 elif file_format == 'json':
     output_json('output', data)
 elif file_format == 'xml':
